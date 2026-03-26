d341f07d256e463652a3ec77f0e75a3d224ee877
Andres Felipe Grimaldos Garcia <grimaldosgarciaa@gmail.com>
2026-03-26 15:06:59 -0500

    Create paises1.java

---
+++ paises1.java
@@ -0,0 +1,19 @@
+package com.mycompany.practicafor.each;
+
+public class paises1 {
+    public static void main(String[]args){
+        String [] paises= new String [8];
+        paises[0]="España";
+        paises[1]="Mexico";
+        paises[2]="Colombia";
+        paises[3]="Peru";
+        paises[4]="Chile";
+        paises[5]="Argentina";
+        paises[6]="Ecuador";
+        paises[7]="Venezuela";
+        
+        for(String elemento:paises)System.out.println("Pais :"+ elemento);
+
+    }
+    
+}
